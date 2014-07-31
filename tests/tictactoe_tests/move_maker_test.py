d5b2bcaa8a81b6d9c9030677880d5379c4865a7d
Peter Borah <peterborah@gmail.com>
2014-07-30 21:22:56 -0500

    Add ability to make moves

---
+++ tests/tictactoe_tests/move_maker_test.py
@@ -0,0 +1,12 @@
+import eth_tools
+from nose.tools import assert_equal
+
+class TestMoveMaker:
+    def setup(self):
+        bc = eth_tools.Blockchain()
+        self.contract = eth_tools.Contract(bc, "contracts/tictactoe/move_maker.se")
+
+    def test_makes_moves(self):
+        before = [2,1,2,1,2,1,0,1,2]
+        after = [2,1,2,1,2,1,1,1,2]
+        assert_equal(self.contract.invoke(before + [1, 6]), after)
