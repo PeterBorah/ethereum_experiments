1fdc0f7cf7b109710f691a46cf0a11f59e781faa
Peter Borah <peterborah@gmail.com>
2014-07-31 23:21:09 -0500

    Allow moves to be made on tictactoe games.

--- tests/tictactoe_tests/tictactoe_test.py
+++ tests/tictactoe_tests/tictactoe_test.py
@@ -7,5 +7,22 @@ class TestTicTacToe:
         self.contract = eth_tools.Contract(bc, "contracts/tictactoe/tictactoe.se")
 
     def test_creates_games(self):
-        assert_equal(self.contract.invoke(["create", "a", "b"]), [1])
-        assert_equal(self.contract.invoke(["get", 1]), [0,0,0,0,0,0,0,0,0,1])
+        game_id = self.contract.invoke(["create", "a", "b"])[0]
+        assert_equal(self.contract.invoke(["get", game_id]), [0,0,0,0,0,0,0,0,0,1])
+
+    def test_moves(self):
+        game_id = self.contract.invoke(["create", "a", "b"])[0]
+        assert_equal(self.contract.invoke(["move",game_id,1,2]), [1])
+        assert_equal(self.contract.invoke(["get",game_id]), [0,0,1,0,0,0,0,0,0,2])
+
+    def test_can_store_multiple_games(self):
+        pass
+
+    def test_only_allows_correct_player_to_move(self):
+        pass
+
+    def test_checks_for_win(self):
+        pass
+
+    def test_checks_for_tie(self):
+        pass
